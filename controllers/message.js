46b22acc5260dc831646e98acf9988805fdab026
dra1n <dra1n86@gmail.com>
2016-03-20 08:39:50 +0200

    Configure cors preflight requests.

--- controllers/message.js
+++ controllers/message.js
@@ -2,6 +2,7 @@ const cors = require('cors');
 const router = require('express').Router();
 const sendMessage = require('../lib/send-message');
 
+router.options('/', cors({origin: '*'}))
 router.post('/', cors({origin: '*'}), (req, res, next) => {
   sendMessage(req.body);
   res.status(200).end();
